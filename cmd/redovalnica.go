5d6e05754e00c0d701720b22180973b925ba1460
gitLimonca <antonija.kompan@gmail.com>
2025-12-02 19:42:48 +0100

    Update redovalnica.go

--- cmd/redovalnica.go
+++ cmd/redovalnica.go
@@ -1,34 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/urfave/cli/v3"
+	"strconv"
 
 	"github.com/gitLimonca/redovalnica-modul/redovalnica"
+	"github.com/urfave/cli/v3"
 )
 
+// Glavni program aplikacije Redovalnica.
+// Podpira naslednje ukaze:
+//   - izpis   : izpiše vse študente in njihove ocene
+//   - dodaj   : doda oceno izbranemu študentu (ukaz: dodaj <vpisna> <ocena>)
+//   - uspeh  : izpiše končni uspeh vseh študentov glede na minimalno število ocen
 func main() {
-
-	// Začetni podatki
+	// Začetni podatki o študentih
 	studenti := map[string]redovalnica.Student{
 		"63210001": {Ime: "Ana", Priimek: "Novak", Ocene: []int{10, 9, 8, 10, 9, 9}},
 		"63210002": {Ime: "Boris", Priimek: "Kralj", Ocene: []int{6, 7, 5, 8, 6, 6}},
 		"63210003": {Ime: "Janez", Priimek: "Novak", Ocene: []int{4, 5, 3, 5, 4, 4}},
 	}
 
-	app := &cli.App{
-		Name:  "Redovalnica",
+	// Definicija ukazov in flagov
+	cmd := &cli.Command{
+		Name:  "redovalnica",
 		Usage: "Aplikacija za upravljanje študentskih ocen",
-
 		Flags: []cli.Flag{
 			&cli.IntFlag{
-				Name:    "stOcen",
-				Value:   6,
-				Usage:   "Minimalno število ocen za izračun končnega uspeha",
-				Aliases: []string{"s"},
+				Name:  "stOcen",
+				Value: 6,
+				Usage: "Minimalno število ocen za izračun končnega uspeha",
 			},
 			&cli.IntFlag{
 				Name:  "minOcena",
@@ -41,58 +45,48 @@ func main() {
 				Usage: "Največja dovoljena ocena",
 			},
 		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			// Preverimo, ali je podan ukaz
+			if cmd.Args().Len() < 1 {
+				fmt.Println("Uporaba: izpis | dodaj <vpisna> <ocena> | uspeh")
+				return nil
+			}
 
-		Commands: []*cli.Command{
-			{
-				Name:  "izpis",
-				Usage: "Izpiše vse študente in njihove ocene",
-				Action: func(c *cli.Context) error {
-					redovalnica.IzpisVsehOcen(studenti)
+			ukaz := cmd.Args().Get(0)
+
+			switch ukaz {
+			case "izpis":
+				// Izpiše vse študente in njihove ocene
+				redovalnica.IzpisVsehOcen(studenti)
+			case "dodaj":
+				// Dodaj oceno študentu
+				if cmd.Args().Len() < 3 {
+					fmt.Println("Uporaba: dodaj <vpisna> <ocena>")
 					return nil
-				},
-			},
-			{
-				Name:      "dodaj",
-				Usage:     "Dodaj oceno izbranemu študentu",
-				ArgsUsage: "<vpisna> <ocena>",
-				Action: func(c *cli.Context) error {
+				}
+				vpisna := cmd.Args().Get(1)
+				ocena, _ := strconv.Atoi(cmd.Args().Get(2))
+				minOcena := cmd.Int("minOcena")
+				maxOcena := cmd.Int("maxOcena")
+				if err := redovalnica.DodajOceno(studenti, vpisna, ocena, minOcena, maxOcena); err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Printf("Dodali smo oceno %d študentu %s\n", ocena, vpisna)
+				}
+			case "uspeh":
+				// Izpiše končni uspeh vseh študentov glede na minimalno število ocen
+				stOcen := cmd.Int("stOcen")
+				redovalnica.IzpisiKoncniUspeh(studenti, stOcen)
+			default:
+				fmt.Println("Neznan ukaz. Na voljo so: izpis, dodaj, uspeh")
+			}
 
-					if c.Args().Len() < 2 {
-						return fmt.Errorf("uporaba: dodaj <vpisna> <ocena>")
-					}
-
-					vpisna := c.Args().Get(0)
-					ocena := c.Args().GetInt(1)
-
-					err := redovalnica.DodajOceno(
-						studenti,
-						vpisna,
-						ocena,
-						c.Int("minOcena"),
-						c.Int("maxOcena"),
-					)
-
-					if err != nil {
-						return err
-					}
-
-					fmt.Println("Ocena uspešno dodana!")
-					return nil
-				},
-			},
-			{
-				Name:  "uspeh",
-				Usage: "Izpiše končni uspeh vseh študentov",
-				Action: func(c *cli.Context) error {
-					redovalnica.IzpisiKoncniUspeh(studenti, c.Int("stOcen"))
-					return nil
-				},
-			},
+			return nil
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
+	// Zaženemo ukaz
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
-
